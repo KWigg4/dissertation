151c21c66514786442e5a7f42cef5eef07277954
Kimberly T Wiggins <64229613+KWigg4@users.noreply.github.com>
2025-09-05 14:24:40 -0600

    I messed something up royally - I'm getting crazy values for sim1 for TOTEXPYY...

--- code/02_meps_process.R
+++ code/02_meps_process.R
@@ -1,16 +1,18 @@
+library(dplyr)
+
 fyc <- readRDS(here::here("data_processed/fyc.RDS")) |>
 	mutate(ind_gt0 = ifelse(TOTEXPYY > 0, 1, 0)) |>
-	mutate(log_totexpy = log(TOTEXPYY), na.rm=TRUE) |>
+	#mutate(log_totexpy = log(TOTEXPYY), na.rm=TRUE) |>
 	# collapse the categories of insurance type (private any, public only)
 	# 3 means uninsured, and it matched UNINSYY
-	mutate(INSCOV = if_else(INSCOVYY ==3, 0, 1)) |>
+	mutate(ind_INSCOV = if_else(INSCOVYY ==3, 0, 1)) |>
 	# race: 2012 only year they didn't combine 4, 5 for RACEV1X. Combine
-	mutate(
-		RACE = case_when(
-			RACEV1X %in% c(4,5) ~ 4,
-			RACEX %in% c(4,5) ~ 4,
-			TRUE ~ coalesce(RACEX, RACEV1X)
-		)) |>
+	# mutate(
+	# 	RACE = case_when(
+	# 		RACEV1X %in% c(4,5) ~ 4,
+	# 		RACEX %in% c(4,5) ~ 4,
+	# 		TRUE ~ coalesce(RACEX, RACEV1X)
+	# 	)) |>
 	# don't need weights, not going to do weighted sampling
 	select(
 		, year:DUPERSID
@@ -18,9 +20,9 @@ fyc <- readRDS(here::here("data_processed/fyc.RDS")) |>
 		, TOTEXPYY
 		, z_TOTEXPYY
 		, ind_gt0
-		, log_totexpy
 		, PANEL
-		, INSCOV
+		, ind_INSCOV
+		, INSCOVYY
 		, INSCOPE   # it has the same values as INSCOPYY
 		, MARRYYYX
 		, fct_MARRYYYX
@@ -28,20 +30,21 @@ fyc <- readRDS(here::here("data_processed/fyc.RDS")) |>
 		, PCS42:MCS42
 		, SEX
 		, HISPANX
-		, RACE
+		# , RACE
 		# , EDUCYR, HIDEG, EDUYRDG, EDRECODE
 		, POVCATYY
 		, fct_POVCATYY
-		, ARTHDX:HIBPDX
-		, RAREDX
+		, ARTHDX:RAREDX
 	) |>
 	mutate(
 		ind_post = ifelse(pre_post=="post", 0, 1),
 		ind_female = ifelse(SEX==2, 1,0),
 		ind_comorb = ifelse(
-			ARTHDX == 1 | ASTHDX==1 | CANCERDX==1 | CHOLDX==1 |
-				DIABDX==1|HIBPDX==1 | RAREDX==1, 1, 0
-		))
+			(ARTHDX == 1 | ASTHDX==1 | CANCERDX==1 | CHOLDX==1 |
+				DIABDX==1|HIBPDX==1 | RAREDX==1), 1, 0),
+		zPCS42 = as.vector(scale(PCS42)),
+		zMCSr2 = as.vector(scale(PCS42))
+		)
 
 write.csv(
 	fyc,
