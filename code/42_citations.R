bb678f00a6f4c60d8a788d6b5df49d85a87f2c80
Kimberly T Wiggins <64229613+KWigg4@users.noreply.github.com>
2026-02-28 13:00:41 -0700

    fixing plots, adding citation code

---
+++ code/42_citations.R
@@ -0,0 +1,24 @@
+citation("ggplot2")
+citation("car")
+
+citation("furrr")
+citation("ggdist")
+citation("patchwork")
+citation("cowplot")
+citation("gt")
+citation("gtsummary")
+print(citation("effectsize"), style="bibtex")
+citation("car")
+citation("emmeans")
+citation("MASS")
+citation("scales")
+citation("forcats")
+citation("glue")
+citation("here")
+citation("lubridate")
+
+citation("base")  # for R itself
+
+citation("gtsummary") # no argument, base R citation (same as above? )
+
+
